0dcb4846a1ee7562984e65766c4d341c3925b7d7
Андрей Чистяков <mr.vp.1999@mail.ru>
2022-01-03 16:30:33 +0500

    в

--- Kursovaya Rabota/Program.cs
+++ Kursovaya Rabota/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Kursovaya_Rabota
 {
-    internal static class Program
+    internal class Program
     {
+        public string connStr = "server=caseum.ru;port=44444;user=st_1_22_19;database=st_1_22_19;password=97035229;";
+        public MySqlConnection conn;
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
